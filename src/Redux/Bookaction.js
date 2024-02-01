2413a133f1a1061a8a8d87a4641dd2c64ef0bccf
Shree Pore <141204446+shree5721@users.noreply.github.com>
2024-02-01 16:26:55 +0530

    Delete src/Redux directory

--- src/Redux/Bookaction.js
+++
@@ -1,9 +0,0 @@
-import { buyBook } from "./BookType";
-
-const Buy_book = () => {
-  return {
-    type: buyBook,
-  };
-};
-
-export default Buy_book;
--- src/Redux/Store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import Bookreducer from "./Bookreducer";
-
-const Store = createStore(Bookreducer);
-
-export default Store;
